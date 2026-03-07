47badd42384d69f7607bfad7c4c33d5fb75c42bf
DanaDvorcakova <danadvorcakova@gmail.com>
2026-03-07 15:35:45 +0000

    urls

--- recipe_project/urls.py
+++ recipe_project/urls.py
@@ -43,5 +43,7 @@ urlpatterns = [
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
     
-if settings.DEBUG: # The media files are only served in debug mode, as we only have an ephemeral file system on Render
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
+
