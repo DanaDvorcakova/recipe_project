6109e079a3f2d4ecfe1bf8b937c286302e3834ee
DanaDvorcakova <danadvorcakova@gmail.com>
2026-03-08 17:07:42 +0000

    ulrs1

--- recipe_project/urls.py
+++ recipe_project/urls.py
@@ -17,8 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
-from django.conf import settings           #New import 1
-from django.conf.urls.static import static #New import 2
+from django.conf import settings           
+from django.conf.urls.static import static 
 from users import views as user_views
 from django.views.static import serve
 from django.urls import re_path
@@ -43,11 +43,11 @@ urlpatterns = [
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-#urlpatterns += [
- #   re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-#]
+urlpatterns += [
+     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
 
 
 
-if settings.DEBUG: # The media files are only served in debug mode, as we only have an ephemeral file system on Render
- urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#if settings.DEBUG: # The media files are only served in debug mode, as we only have an ephemeral file system on Render
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
