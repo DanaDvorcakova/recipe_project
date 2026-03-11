eefaa178fe2d931e44ab224f89e124bcb708ad3b
DanaDvorcakova <danadvorcakova@gmail.com>
2026-03-11 15:20:13 +0000

    updating post class

--- blog/models.py
+++ blog/models.py
@@ -1,17 +1,27 @@
-# blog/models.py
-
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from cloudinary_storage.storage import MediaCloudinaryStorage
 from cloudinary.utils import cloudinary_url
 from django.templatetags.static import static
 
 # -------------------- Recipe Post --------------------
 class Post(models.Model):
-    CATEGORY_CHOICES = [...]
-    STATUS_CHOICES = [...]
+    CATEGORY_CHOICES = [
+        ('Appetizer', 'Appetizer'),
+        ('Main Course', 'Main Course'),
+        ('Dessert', 'Dessert'),
+        ('Soup', 'Soup'),
+    ]
 
+    STATUS_CHOICES = [
+        ('Draft', 'Draft'),
+        ('Published', 'Published'),
+        ('Archived', 'Archived'),
+    ]
+
+    # Required Fields
     title = models.CharField(max_length=100)
     description = models.TextField()
     start_date = models.DateField(default=timezone.now)
@@ -19,14 +29,21 @@ class Post(models.Model):
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='Draft')
     stakeholders = models.ManyToManyField(User, related_name='contributed_posts', blank=True)
 
+    # Recipe Fields
     ingredients = models.TextField()
     instructions = models.TextField()
-    cooking_time = models.PositiveIntegerField()
+    cooking_time = models.PositiveIntegerField(help_text="Cooking time in minutes")
     category = models.CharField(max_length=50, choices=CATEGORY_CHOICES, default='Main Course')
-    image = models.ImageField(upload_to='post_images/', blank=True, null=True)
+    image = models.ImageField(
+        upload_to='post_images/',
+        storage=MediaCloudinaryStorage(),
+        blank=True,
+        null=True,
+    )
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
 
+    # Like & Save functionality
     likes = models.ManyToManyField(User, related_name='liked_posts', blank=True)
     saved_by = models.ManyToManyField(User, related_name='saved_posts', blank=True)
 
@@ -38,9 +55,18 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-    
+
     def get_absolute_url(self):
         return reverse('post-detail', kwargs={'pk': self.pk})
+
+    def get_image_url(self):
+        """
+        Returns the Cloudinary URL if image exists, otherwise fallback to static default.
+        """
+        if self.image:
+            return cloudinary_url(self.image.name, width=800, height=600, crop="fill")[0]
+        return static('blog/images/default_recipe.jpg')
+
 
 # -------------------- Comments --------------------
 class Comment(models.Model):
@@ -54,14 +80,3 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"{self.user.username} - {self.post.title}"
-
-# -------------------- Profile --------------------
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='profile_pics', null=True)
-
-    def get_image_url(self):
-        if self.image:
-            return cloudinary_url(self.image.name, width=300, height=300, crop="lfill")[0]
-        else:
-            return static('blog/images/image1.jpg')
