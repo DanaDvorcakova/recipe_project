5e4dd0e2847d43478080395c6428bfda3e31443c
DanaDvorcakova <danadvorcakova@gmail.com>
2026-03-25 11:38:23 +0000

    update

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -124,14 +124,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 STATICFILES_STORAGE = "whitenoise.storage.ManifestStaticFilesStorage"
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 
-
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
